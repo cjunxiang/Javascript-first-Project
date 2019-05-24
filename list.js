51b1307d1b21692635ed46c4e07ae8a1e8b7173c
cjunxiang <e0176013@u.nus.edu>
2019-05-24 11:07:55 +0800

    Entered placeholders for functions

---
+++ list.js
@@ -0,0 +1,24 @@
+
+let newTask = document.getElementbyId("new-task");
+
+
+let createNewTask = function(taskString) {
+
+}
+
+let addTask = function() {
+	console.log("adding Task...");
+}
+
+let editTask = function() {
+	console.log("editting Task...");
+}
+
+let deleteTask = function() {
+	console.log("deleting Task...");
+}
+
+let taskCompleted = function() {
+	console.log("Completing Task...");
+}
+
