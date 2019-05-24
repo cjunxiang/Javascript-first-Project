36a911d5fd778a20b95dc1a8d175b6773dd6c1c4
cjunxiang <e0176013@u.nus.edu>
2019-05-24 11:18:43 +0800

    Added elements to new "TODO"

--- list.js
+++ list.js
@@ -1,8 +1,21 @@
 
 let newTask = document.getElementbyId("new-task");
+let buttonPressed = document.getElementsByTagName("button")[0];
 
+
+//UL unordered list
+// li list item
 
 let createNewTask = function(taskString) {
+	// new task under todo should have: [checkbox]||name||edit||delete
+	let newListItem = document.createElement("li");
+
+	let checkBox = document.createElement("input");
+	checkBox.type = "checkbox";
+	let nameHolder = document.createElement("label");
+	let editInput = document.createElement("input"); 
+	editInput.type = "text";
+    let editButton = document.createElement("button"); 
 
 }
 
