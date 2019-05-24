9b9a320eeec0b9a0eb488f4cafb5679db2e97898
cjunxiang <e0176013@u.nus.edu>
2019-05-24 11:28:18 +0800

    createNewTask
    creates new list from input

--- list.js
+++ list.js
@@ -2,21 +2,38 @@
 let newTask = document.getElementbyId("new-task");
 let buttonPressed = document.getElementsByTagName("button")[0];
 
-
 //UL unordered list
 // li list item
+let incompleteTaskHolder = document.getElementbyId("incomplete-tasks");
+let completedTaskHolder = document.getElementbyId("completed-tasks")
 
+//new list constructor (creates a new list)
 let createNewTask = function(taskString) {
-	// new task under todo should have: [checkbox]||name||edit||delete
+	/* new task under todo should have: 
+	[checkbox]||name||edit||delete */
 	let newListItem = document.createElement("li");
 
 	let checkBox = document.createElement("input");
 	checkBox.type = "checkbox";
+
 	let nameHolder = document.createElement("label");
+	nameHolder.innerText = taskString;
+
 	let editInput = document.createElement("input"); 
 	editInput.type = "text";
-    let editButton = document.createElement("button"); 
 
+    let editButton = document.createElement("button"); 
+    editButton.innerText = "Edit";
+    editButton.className = "Edit";
+
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.className =  "delete";
+
+    listItem.appendChild(checkBox).appendChild(nameHolder)
+    .appendchild(editInput).appendChild(editButton).append(deleteButton);
+
+    return listItem;
 }
 
 let addTask = function() {
